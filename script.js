6baa5f843bce94d103eb562b0f8582dbc00b666a
mgibson <matthew.jgibson43@gmail.com>
2023-06-08 06:23:31 -0400

    Update: Append, size, head, and tail functions

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
+'use strict'
+
 class LinkedList {
   constructor() {
     this.head = null;
-    this.size = 0;
+    this.length = 0;
   }
 
   append(value) {
+    // Create new node
     const node = new Node(value);
-    if (head === null) {
+
+    // Initialize variable to store current node
+    let current;
+
+    // If list is empty, add node to head
+    // If list is not empty, iterate to end of list
+    if (this.head === null) {
       this.head = node;
     } else {
-      
+      current = this.head;
+
+      // Iterates to end of list
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
     }
-    this.size++;
+
+    // Increments list size
+    this.length++;
   }
 
+  // Returns size of list
   size() {
-    return this.size;
+    return this.length;
   }
 
+  // Returns head of list
   head() {
-    return this.head.value;
+    return this.head;
   }
 
   tail() {
-    
+    let current = this.head;
+
+    // Iterate to end of list
+    while (current.next) {
+      current = current.next;
+    }
+
+    // Returns last item in list
+    return current;
   }
 }
 
@@ -33,3 +61,9 @@ class Node {
     this.next = null;
   }
 }
+
+const list = new LinkedList();
+list.append('cat');
+list.append('dog');
+list.append('horse');
+console.log(list.size());
